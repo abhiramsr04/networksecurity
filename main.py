4184d7d7efab84cf2836888bec91cefb3441a916
Abhiram S R <abhiramsr173@gmail.com>
2025-07-25 14:56:29 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 
 import sys
 
@@ -13,7 +14,15 @@ if __name__ == "__main__":
         logging.info("Initiated data ingestion component")
         dataingestion.initiate_data_ingestion()
         dataingestionartifact = dataingestion.initiate_data_ingestion()
+        logging.info("Data ingestion completed successfully")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_vallidation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiated data validation component")
+        data_vallidation.initiate_data_validation()
+        data_validation_artifact = data_vallidation.initiate_data_validation()
+        logging.info("Data validation completed successfully")
+        print(data_validation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -33,7 +33,7 @@ class DataIngestion:
             self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
             collection = self.mongo_client[database_name][collection_name]
 
-            df = pd.DataFrame(list(collection.find({})))
+            df = pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
                 df = df.drop(columns=["_id"], axis=1)
 
